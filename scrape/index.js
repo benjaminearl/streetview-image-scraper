ec49b10e99d73aebdd3d02c6b9625d21e1ac24d8
benjaminearl <bnjmnearl@gmail.com>
2018-05-13 17:02:12 +0200

    Add first files to repository

---
+++ scrape/index.js
@@ -0,0 +1,39 @@
+var request = require('request');
+var fs = require('fs');
+
+let west = 4.291945;
+let east = 4.319143;
+let north = 52.074047;
+let south = 52.061590;
+
+var latitudeList = [];
+var longitudeList = [];
+
+while(west < east){
+    var nextLat = west += 0.000730;
+    latitudeList.push(nextLat)
+};
+
+while(north > south){
+    var nextLong = north -= 0.000450;
+    longitudeList.push(nextLong);
+};
+
+var coordinates = [];
+
+for (var i = 0; i < latitudeList.length; i++){
+    for(var j = 0; j < longitudeList.length; j++){
+        coordinates.push(`${longitudeList[j]},${latitudeList[i]}`);
+    };
+};
+
+for (var i = 0; i < coordinates.length; i++) {
+    var download = function(uri, filename, callback){
+            request.head(uri, function(err, res, body){
+                request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+        });
+    }
+    download(`https://maps.googleapis.com/maps/api/streetview?location=${coordinates[i]}&size=456x456key=AIzaSyB4I4Q-kAff_pEK869igqNhN5xtc0qKpo4`, `${coordinates[i]}.jpg`, function(){
+        console.log(coordinates[i]);
+    });
+};
